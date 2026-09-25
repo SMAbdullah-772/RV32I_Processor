f493f67d5a9abf94218fdc3c3321bba067562497
SMAbdullah-772 <smabdullah772@gmail.com>
2026-09-25 11:38:49 +0500

    Imem Logic Redefined for slicing bits from PC register

--- RV32i_Verilog/IMEM.v
+++ RV32i_Verilog/IMEM.v
@@ -1,9 +1,8 @@
-// Code your design here
 module imem (
-    input  wire [31:0] addr,       // = PC
+    input  wire [31:0] addr,
     output wire [31:0] instr
 );
-    reg [31:0] mem [0:1023];       // size to taste
+    reg [31:0] mem [0:1023];
     initial $readmemh("program.hex", mem);
-    assign instr = mem[addr[31:2]];  // word-aligned: PC/4 as the index
+    assign instr = mem[addr[11:2]];  
 endmodule
